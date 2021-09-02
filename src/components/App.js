7accd04a838b247977a0bac2774d6e576cf22d86
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-02 21:06:16 +0530

    Created an async function to fetch data from youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube"
 class App extends React.Component {
 
-    onUserSearchTermSubmit = (userSearchTerm) => {
+    state = { videosDataFromRequest: [] };
+
+    onUserSearchTermSubmit = async (userSearchTerm) => {
+        const apiResponse = await youtube.get('/search', {
+            params: {
+                q: userSearchTerm
+            }
+        });
+
+        this.setState({ videosDataFromRequest: apiResponse.data.items });
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onUserSearchTermSubmit} />
+                I have {this.state.videosDataFromRequest.length} videos.
             </div >
         );
     }
