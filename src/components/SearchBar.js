2b6ec750cdaad57315176646904e4a02b51a5e0c
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-02 18:04:00 +0530

    Added boiler plate code for basic structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div className="search-bar">
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
