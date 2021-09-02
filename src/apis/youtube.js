471d6b194b7743d4fa0a3555a22b28fa4263d921
Adarsh V Desai <desaiadarsh22@gmail.com>
2021-09-02 20:14:54 +0530

    corrected mistake in baseURL

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'AIzaSyC4bB9_XKrzfqt1aIY3yhA2ME_PBDA-rao';
 
 export default axios.create({
-    baseURL: 'GET https://www.googleapis.com/youtube/v3/search',
+    baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
